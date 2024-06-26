186875a62b5637270fef61cf8a86328074f714dd
totsamyroma <romantehanov@gmail.com>
2024-06-26 12:18:49 +0300

    Readme

--- metrics_fe/src/app/home-wrapper.tsx
+++ metrics_fe/src/app/home-wrapper.tsx
@@ -54,7 +54,7 @@ export default function HomeWrapper({
         </Link>
 
         <Link
-          href="https://github.com"
+          href="https://github.com/totsamyroma/metrics"
           target="_blank"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
         >
